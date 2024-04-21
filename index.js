ebccc5485b67658ba9e3554aa1097e19cc66f256
Eejit <76887639+Eejit43@users.noreply.github.com>
2024-04-21 17:41:18 -0400

    refactor: use eslint flat config

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
-module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        'plugin:@typescript-eslint/stylistic-type-checked',
-        'plugin:jsdoc/recommended-typescript',
-        'plugin:unicorn/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', '@stylistic/ts', 'jsdoc', 'unicorn'],
-    root: true,
-    rules: {
-        // TypeScript ESLint
-        '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-        '@typescript-eslint/default-param-last': 'error',
-        '@typescript-eslint/dot-notation': 'error',
-        '@typescript-eslint/naming-convention': [
-            'error',
-            { selector: ['default', 'variableLike', 'method'], format: ['strictCamelCase', 'UPPER_CASE'] },
-            { selector: ['default', 'variableLike', 'method'], format: null, filter: { regex: '^(\\d+|Querystring|Params|Body)$', match: true } },
-            { selector: 'typeLike', format: ['StrictPascalCase'] },
-            { selector: 'memberLike', format: null, filter: { regex: '[-]', match: true } },
-            { selector: 'import', format: ['camelCase', 'PascalCase'] },
-        ],
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/no-misused-promises': 'off',
-        '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-        '@typescript-eslint/no-unused-expressions': 'error',
-        '@typescript-eslint/non-nullable-type-assertion-style': 'off',
-        '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true, allowBoolean: true }],
+// @ts-check
 
-        // ESLint Stylistic (TS)
-        '@stylistic/ts/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+import eslint from '@eslint/js';
+import stylisticTypescript from '@stylistic/eslint-plugin-ts';
+import jsdoc from 'eslint-plugin-jsdoc';
+import unicorn from 'eslint-plugin-unicorn';
+import typescriptEslint from 'typescript-eslint';
 
-        // JSDoc
-        'jsdoc/check-indentation': 'warn',
-        'jsdoc/require-description-complete-sentence': 'warn',
-        'jsdoc/require-description': 'warn',
-        'jsdoc/require-hyphen-before-param-description': ['warn', 'never'],
-        'jsdoc/require-returns': 'off',
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+    {
+        languageOptions: { ecmaVersion: 'latest', parser: typescriptEslint.parser },
+        plugins: { '@typescript-eslint': typescriptEslint.plugin, '@stylistic/ts': stylisticTypescript, jsdoc, unicorn },
+        rules: {
+            // Config extensions
+            ...eslint.configs.recommended.rules,
+            ...typescriptEslint.configs.eslintRecommended,
+            ...typescriptEslint.configs.recommendedTypeChecked,
+            ...typescriptEslint.configs.stylisticTypeChecked,
+            ...jsdoc.configs['flat/recommended'].rules,
+            ...unicorn.configs['flat/recommended'].rules,
 
-        // Unicorn
-        'unicorn/no-await-expression-member': 'off',
-        'unicorn/no-nested-ternary': 'off',
-        'unicorn/no-null': 'off',
+            // TypeScript ESLint
+            '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+            '@typescript-eslint/default-param-last': 'error',
+            '@typescript-eslint/dot-notation': 'error',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                { selector: ['default', 'variableLike', 'method'], format: ['strictCamelCase', 'UPPER_CASE'] },
+                { selector: ['default', 'variableLike', 'method'], format: null, filter: { regex: '^(\\d+|Querystring|Params|Body)$', match: true } },
+                { selector: 'typeLike', format: ['StrictPascalCase'] },
+                { selector: 'memberLike', format: null, filter: { regex: '[-]', match: true } },
+                { selector: 'import', format: ['camelCase', 'PascalCase'] },
+            ],
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-misused-promises': 'off',
+            '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+            '@typescript-eslint/no-unused-expressions': 'error',
+            '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+            '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true, allowBoolean: true }],
 
-        // ESLint
-        curly: ['error', 'multi'],
-        'default-case-last': 'error',
-        'default-param-last': 'off',
-        'dot-notation': 'off',
-        eqeqeq: ['error', 'always'],
-        'no-await-in-loop': 'error',
-        'no-console': ['error', { allow: ['warn', 'error'] }],
-        'no-lonely-if': 'error',
-        'no-template-curly-in-string': 'error',
-        'no-unneeded-ternary': 'error',
-        'no-unused-expressions': 'off',
-        'no-useless-concat': 'error',
-        'object-shorthand': 'error',
-        'prefer-arrow-callback': 'error',
-        'prefer-destructuring': ['error', { array: false, object: true }, { enforceForRenamedProperties: false }],
-        yoda: 'error',
+            // ESLint Stylistic (TS)
+            '@stylistic/ts/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+
+            // JSDoc
+            'jsdoc/check-indentation': 'warn',
+            'jsdoc/require-description-complete-sentence': 'warn',
+            'jsdoc/require-description': 'warn',
+            'jsdoc/require-hyphen-before-param-description': ['warn', 'never'],
+            'jsdoc/require-returns': 'off',
+
+            // Unicorn
+            'unicorn/no-await-expression-member': 'off',
+            'unicorn/no-nested-ternary': 'off',
+            'unicorn/no-null': 'off',
+
+            // ESLint
+            curly: ['error', 'multi'],
+            'default-case-last': 'error',
+            'default-param-last': 'off',
+            'dot-notation': 'off',
+            eqeqeq: ['error', 'always'],
+            'no-await-in-loop': 'error',
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+            'no-lonely-if': 'error',
+            'no-template-curly-in-string': 'error',
+            'no-unneeded-ternary': 'error',
+            'no-unused-expressions': 'off',
+            'no-useless-concat': 'error',
+            'object-shorthand': 'error',
+            'prefer-arrow-callback': 'error',
+            'prefer-destructuring': ['error', { array: false, object: true }, { enforceForRenamedProperties: false }],
+            yoda: 'error',
+        },
     },
-};
+];
